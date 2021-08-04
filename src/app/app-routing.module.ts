0e077d09d7c136ff876d493bf37878123649839e
Kamil Wisniewski <kamil.m.wis@gmail.com>
2021-08-04 16:31:19 +0200

    Created english component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PolishComponent} from "./components/polish/polish.component";
+import {EnglishComponent} from "./components/english/english.component";
 
 const routes: Routes = [
+  {
+    path: "polish",
+    component: PolishComponent
+  },
+  {
+    path: "english",
+    component: EnglishComponent
+  },
   {
     path: "**",
     redirectTo: "polish",
     pathMatch: "full"
-  },
-  {
-    path: "polish",
-    component: PolishComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PolishComponent } from './components/polish/polish.component';
+import { EnglishComponent } from './components/english/english.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PolishComponent
+    PolishComponent,
+    EnglishComponent
   ],
   imports: [
     BrowserModule,
